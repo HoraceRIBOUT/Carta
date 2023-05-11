c114417b6584b72e5d018b55b925b05ff57cc25b
DESKTOP-EA4FO0M\Horace <pifi77@hotmail.fr>
2023-05-11 14:50:23 +0200

    No inventory during map and paper

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -37,8 +37,6 @@ public class InputManager : MonoBehaviour
         }
         else if (GameManager.instance.mapAndPaper.mapOpen)
         {
-            if (!GameManager.instance.dialogMng.inventoryBlock)
-                Try_OpenInventory();
             Try_CloseMapAndPaper();
 
         }
